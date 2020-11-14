17b64dd8fd551352173d00975770bd884a950c81
Alexander Chaykovskiy <fosswolfgang@hotmail.com>
2020-11-14 15:50:57 +0000

    14/11/2020 До правок в поле

--- main.cs
+++ main.cs
@@ -1,33 +1,18 @@
 using System;
+using System.Collections.Generic;
 
-  class Game{
-    public Game(){
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+       try{
+				Game game = new Game();
+				game.Start();
+			 }
+			 catch(Exception ex){
+				 Console.WriteLine(ex);
+			 }
 
     }
-    public void Start(){
-      Console.Clear();
-      Random random = new Random(); 
-      int value = random.Next(1,100);
-      bool isFinished = false;  
-      for(int i=0;!isFinished;i++){
-         int guess=Convert.ToInt32(Console.ReadLine());
-        if(value>guess) 
-          Console.WriteLine("Ваше число меньше");
-        if(value<guess)
-        Console.WriteLine("Ваше число больше");
-        if(value==guess){
-          Console.WriteLine("Вы угадали!");
-          Console.WriteLine(i);
-          isFinished=true;          
-          Console.ReadKey();
-        }
-      }
-    }
-  }
-
-  class Program {
-    public static void Main (string[] args) {
-     Game game = new Game();
-     game.Start();
-    }
-  }
+}
